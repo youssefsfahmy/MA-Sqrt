7b4276350bf02e3a26c6b184dec03c3ef576e4ba
Abdelrahman Amin <abdelrahmanadsl@yahoo.com>
2021-08-25 15:53:09 +0200

    architecture

---
+++ src/Testlinks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Testlinks() {
+    return (
+        <div>
+            <Link to = ""> 123</Link>
+            <Link to = "Login"> 234</Link>
+            <Link to = "Signup">4324 </Link>
+            <Link to = "Notes"> 432413</Link>
+            <Link to = "Todo"> 9847</Link>
+        </div>
+    )
+}
