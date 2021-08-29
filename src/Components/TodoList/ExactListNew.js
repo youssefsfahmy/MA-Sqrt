1a078fb94e8f82246c0663e2c2934474efd7b799
Abdallah <abdallahh.saleh00@gmail.com>
2021-08-29 14:34:31 +0200

    finalTodo

--- src/Components/TodoList/ExactListNew.js
+++ src/Components/TodoList/ExactListNew.js
@@ -4,13 +4,16 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
+// import CommentIcon from '@material-ui/icons/Comment';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { Filter } from '@material-ui/icons';
-import reactDom from 'react-dom';
+// import { Filter } from '@material-ui/icons';
+// import reactDom from 'react-dom';
 import DoneIcon from '@material-ui/icons/Done';
+// import Textbox1 from './Textbox1'
+import EditText from './EditText'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,7 +26,7 @@ const useStyles = makeStyles(theme => ({
 function CheckboxList(props) {
   const classes = useStyles();
   const [checked, setChecked] = React.useState([]);
-
+  // const [edited,setEdited]
 
 
   const handleDelete=(id) => {
@@ -32,7 +35,10 @@ function CheckboxList(props) {
       console.log('ay haga')
       console.log(id)
     }
-  
+    const onChange1=(e,id) => {
+      const elem = props.arrayy[id]
+      props.setArrayy([...props.arrayy.slice(0,id),{...elem,text:e.target.value},...props.arrayy.slice(id+1)])
+      }
   
 
   const handleCheck = (id) =>{
@@ -43,6 +49,11 @@ function CheckboxList(props) {
   const handleEdit=(id) =>{
     const elem = props.arrayy[id]
     props.setArrayy([...props.arrayy.slice(0,id),{...elem,editMode:true},...props.arrayy.slice(id+1)])
+  }
+  const handleUpdate=(id) =>{
+    const elem = props.arrayy[id]
+    props.setArrayy([...props.arrayy.slice(0,id),{...elem,editMode:false},...props.arrayy.slice(id+1)])
+
   }
   
 
@@ -61,22 +72,33 @@ function CheckboxList(props) {
   };
 
   return (
+    <>
+    <div/>
     <List className={classes.root}>
       {props.arrayy.map((elem,index) => (
         <ListItem key={elem} role={undefined} dense button onClick={()=>handleToggle(elem)}>
           {/* <Checkbox checked={ checked.indexOf(elem.isComplete) !== -1} tabIndex={-1} disableRipple  onClick={() => handleCheck(elem.key)}   id={elem.key}/> */}
           <Checkbox checked={elem.isComplete} tabIndex={-1} disableRipple  onClick={() => handleCheck(index)}   id={elem.key}/>
 
-          <ListItemText className="listText"  primary={elem.text} onClick={() => handleEdit(index)} id={elem.key} />
+          { elem.editMode===false?    <ListItemText className="listText"  primary={elem.text} onClick={() => handleEdit(index)} id={elem.key} />
+          :<div className="divEdit">
+             <TextField id="standard-basic" label="" value={elem.text} onChange={(e)=>onChange1(e, index)}/>
+             {/* <EditText/> */}
+             <IconButton aria-label="Comments" onClick={() => handleUpdate(elem.key)} id={elem.key}>
+              <DoneIcon/>
+            </IconButton>   
+
+          </div>    }
           <ListItemSecondaryAction>
             <IconButton aria-label="Comments" onClick={() => handleDelete(elem.key)} id={elem.key}>
-              
               <DeleteIcon/>
             </IconButton>
           </ListItemSecondaryAction>
         </ListItem>
       ))}
     </List>
+    <div/>
+    </>
   );
 }
 
