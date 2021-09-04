e6205d11b29aedd02175dfe476f2ede7f4eb8b23
abdallahabdellatif <abdallah.abdellatif00@gmail.com>
2021-09-04 22:15:44 +0200

    edits

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 // import logo from "./logo.svg";
-import "./App.css";
-import {Route, Switch } from "react-router-dom";
-import Homepage from "./Pages/Homepage";
-import Login from "./Pages/Login";
-import SignUp from "./Pages/SignUp";
-import Notes from "./Pages/Notes";
-import Todo from "./Pages/Todo";
-import Testlinks from "./Testlinks";
-import UserNamecontext from "./Components/UserNamecontext";
-import React from "react";
-import Titlecontext from "./Components/TodoList/Titlecontext";
+import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import Homepage from './Pages/Homepage'
+import Login from './Pages/Login'
+import SignUp from './Pages/SignUp'
+import Notes from './Pages/Notes'
+import Todo from './Pages/Todo'
+import Testlinks from './Testlinks'
+import UserNamecontext from './Components/UserNamecontext'
+import React from 'react'
+import Titlecontext from './Components/TodoList/Titlecontext'
 function App() {
-  const [user, setUser] = React.useState("Maya");
-  const [title, setTitle] = React.useState("Untitled");
+  const [user, setUser] = React.useState('Maya')
+  const [title, setTitle] = React.useState('Untitled')
   return (
     <main>
       <UserNamecontext.Provider value={[user, setUser]}>
         <Titlecontext.Provider value={[title, setTitle]}>
           <Testlinks />
           <Switch>
-            <Route path="/" component={Homepage} exact />
-            <Route path="/Login" component={Login} exact />
-            <Route path="/Signup" component={SignUp} exact />
-            <Route path="/Notes" component={Notes} exact />
-            <Route path="/Todo" component={Todo} exact />
+            <Route path='/Home' component={Homepage} exact />
+            <Route path='/' component={Login} exact />
+            <Route path='/Signup' component={SignUp} exact />
+            <Route path='/Notes' component={Notes} exact />
+            <Route path='/Todo' component={Todo} exact />
           </Switch>
         </Titlecontext.Provider>
       </UserNamecontext.Provider>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/LogIn/SignIn.js
+++ src/Components/LogIn/SignIn.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import createContext from "react";
-import { useContext } from "react";
-import UserNamecontext from "../UserNamecontext";
-import { useHistory } from "react-router-dom";
-import user from "../UserNamecontext";
-import { useState } from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import createContext from 'react'
+import { useContext } from 'react'
+import UserNamecontext from '../UserNamecontext'
+import { useHistory } from 'react-router-dom'
+import user from '../UserNamecontext'
+import { useState } from 'react'
 // import { History } from "history";
 export default function SignIn() {
-  const [name, setName] = React.useState("");
-  const [user, setUser] = useContext(UserNamecontext);
-  const history = useHistory();
+  const [name, setName] = React.useState('')
+  const [user, setUser] = useContext(UserNamecontext)
+  const history = useHistory()
   function handlelogin() {
-    let path = "/";
-    setUser(name);
-    history.push(path);
+    let path = '/Home'
+    setUser(name)
+    history.push(path)
   }
   return (
     <div
       style={{
-        justifyContent: "center",
-        width: "30vw",
-        height: "40vw",
-        marginLeft: "40vw",
-        paddingTop: "10vw",
-        backgroundColor: "white",
+        justifyContent: 'center',
+        width: '30vw',
+        height: '40vw',
+        marginLeft: '40vw',
+        paddingTop: '10vw',
+        backgroundColor: 'white',
       }}
     >
       <h1
         style={{
-          marginLeft: "10vw",
-          fontFamily: "sans-serif",
-          fontWeight: "800",
+          marginLeft: '10vw',
+          fontFamily: 'sans-serif',
+          fontWeight: '800',
         }}
       >
         SIGN IN
@@ -39,54 +39,54 @@ export default function SignIn() {
       <div>
         <label
           style={{
-            fontWeight: "600",
-            fontFamily: "sans-serif",
-            paddingLeft: "5vw",
+            fontWeight: '600',
+            fontFamily: 'sans-serif',
+            paddingLeft: '5vw',
           }}
         >
-          Phone OR Email{" "}
+          Phone OR Email{' '}
         </label>
       </div>
       <input
         style={{
-          marginLeft: "5vw",
-          marginBottom: "2vw",
-          height: "2vw",
-          width: "20vw",
+          marginLeft: '5vw',
+          marginBottom: '2vw',
+          height: '2vw',
+          width: '20vw',
         }}
-        type="text"
+        type='text'
         onChange={(event) => setName(event.target.value)}
       />
       <div>
         <label
           style={{
-            fontWeight: "600",
-            fontFamily: "sans-serif",
-            paddingLeft: "5vw",
+            fontWeight: '600',
+            fontFamily: 'sans-serif',
+            paddingLeft: '5vw',
           }}
         >
-          Password{" "}
+          Password{' '}
         </label>
       </div>
       <input
         style={{
-          marginLeft: "5vw",
-          marginBottom: "2vw",
-          height: "2vw",
-          width: "20vw",
+          marginLeft: '5vw',
+          marginBottom: '2vw',
+          height: '2vw',
+          width: '20vw',
         }}
-        type="text"
+        type='text'
       />
       <div>
         <button
           style={{
-            backgroundColor: "white",
-            border: "none",
-            color: "#ED1C24",
-            fontWeight: "300",
-            fontSize: "1vw",
-            paddingLeft: "5vw",
-            marginBottom: "2.5vw",
+            backgroundColor: 'white',
+            border: 'none',
+            color: '#ED1C24',
+            fontWeight: '300',
+            fontSize: '1vw',
+            paddingLeft: '5vw',
+            marginBottom: '2.5vw',
           }}
         >
           Forget Password?
@@ -95,15 +95,15 @@ export default function SignIn() {
       <div>
         <button
           style={{
-            border: "none",
-            marginLeft: "5vw",
-            width: "20vw",
-            height: "2vw",
-            backgroundColor: "#ED1C24",
-            color: "white",
-            fontWeight: "700",
-            fontSize: "1vw",
-            fontFamily: "sans-serif",
+            border: 'none',
+            marginLeft: '5vw',
+            width: '20vw',
+            height: '2vw',
+            backgroundColor: '#ED1C24',
+            color: 'white',
+            fontWeight: '700',
+            fontSize: '1vw',
+            fontFamily: 'sans-serif',
           }}
           onClick={handlelogin}
         >
@@ -111,19 +111,19 @@ export default function SignIn() {
         </button>
       </div>
       <div>
-        <Link to="/SignUp">
+        <Link to='/SignUp'>
           <button
             style={{
-              border: "none",
-              marginLeft: "5vw",
-              marginTop: "1.5vw",
-              width: "20vw",
-              height: "2vw",
-              backgroundColor: "#ED1C24",
-              color: "white",
-              fontWeight: "700",
-              fontSize: "1vw",
-              fontFamily: "sans-serif",
+              border: 'none',
+              marginLeft: '5vw',
+              marginTop: '1.5vw',
+              width: '20vw',
+              height: '2vw',
+              backgroundColor: '#ED1C24',
+              color: 'white',
+              fontWeight: '700',
+              fontSize: '1vw',
+              fontFamily: 'sans-serif',
             }}
           >
             SIGN UP
@@ -131,5 +131,5 @@ export default function SignIn() {
         </Link>
       </div>
     </div>
-  );
+  )
 }
