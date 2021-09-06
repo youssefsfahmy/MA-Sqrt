434635abce552137ba7c91b9bc47315d693e0bbc
Abdallah <abdallahh.saleh00@gmail.com>
2021-09-06 16:16:54 +0200

    .

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -2,22 +2,32 @@ import React, { useContext } from "react";
 import Textbox1 from "./Textbox1";
 import ExactlistNew from "./ExactListNew";
 import Titlecontext from "./Titlecontext";
+import MuiAlert from "@material-ui/lab/Alert";
+import Snackbar from "@material-ui/core/Snackbar";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function TodoList(props) {
-  // console.log(props.cur);
   const [add, setAdd] = React.useState(false);
   const [title, setTitle] = useContext(Titlecontext);
+  const [open, setOpen] = React.useState(false); //snackbar
+  const [popup, setPopup] = React.useState({ message: "", severity: "" });
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+
   const [array, setArray] = React.useState(
     props.cur === -1 ? [] : props.all[props.cur].arrTodos
   );
-  // console.log("AAAAA", array);
   React.useEffect(() => {
     if (props.cur === -1) return;
-    // console.log(array);
-    // const arrTodos = props.cur === -1 ? [] : props.all[props.cur].arrTodos;
-    // console.log(props.cur + " " + arrTodos);
-    // setArray(arrTodos);
-    // console.log(props.all[props.cur].title)
     setTitle(props.all[props.cur].title);
     props.setAll([
       ...props.all.slice(0, props.cur),
@@ -28,7 +38,6 @@ export default function TodoList(props) {
   React.useEffect(() => {
     if (props.cur === -1) return;
     setArray(props.all[props.cur].arrTodos);
-   /// console.log(props.all[props.cur].arrTodos)
   }, [props.cur]);
   return (
     <div
@@ -49,6 +58,11 @@ export default function TodoList(props) {
           <ExactlistNew arrayy={array} setArrayy={setArray} />
         </div>
       </div>
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={popup.severity}>
+          {popup.message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
