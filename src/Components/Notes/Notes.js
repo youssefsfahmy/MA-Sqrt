97cfd78dcf42f2cd94c4a2559996be7df5c955bc
abdallahabdellatif <abdallah.abdellatif00@gmail.com>
2021-09-06 16:14:10 +0200

    began in notes

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -1,47 +1,44 @@
-import React from "react";
-import NoteCard from "./NoteCard";
-import NoteInput from "../../Components/Notes/NoteInput";
-import { makeStyles } from "@material-ui/core/styles";
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
+import React from 'react'
+import NoteCard from './NoteCard'
+import NoteInput from '../../Components/Notes/NoteInput'
+import { makeStyles } from '@material-ui/core/styles'
+import Snackbar from '@material-ui/core/Snackbar'
+import MuiAlert from '@material-ui/lab/Alert'
+import axios from 'axios'
 function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} variant='filled' {...props} />
 }
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex", 
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     // justifyContent: "center",
-    minHeight: "100vh",
-    backgroundColor: "mintcream",
+    minHeight: '100vh',
+    backgroundColor: 'mintcream',
   },
   notes: {
     // display: "flex",
     // flexWrap: "wrap",
-    marginTop: "3vw",
+    marginTop: '3vw',
     // placeContent:"center",
     // alignItems:"center"
   },
-  noteSection:{
-       display: "flex",
-       flexWrap: "wrap",
-       placeContent:"center",
-
-  }
-}));
+  noteSection: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    placeContent: 'center',
+  },
+}))
 export default function Notes() {
-  const classes = useStyles();
-  const [arr, setArr] = React.useState([]);
-  // const [el, setEl] = React.useState();
-  const [button, setButton] = React.useState(false);
-  const [id, setId] = React.useState(0);
-  const [open, setOpen] = React.useState(false);
-//   const [open2, setOpen2] = React.useState(false);
-//   const [open3, setOpen3] = React.useState(false);
-//   const [open4, setOpen4] = React.useState(false);
+  const classes = useStyles()
+  const [arr, setArr] = React.useState([])
+  const [button, setButton] = React.useState(false)
+  const [id, setId] = React.useState(0)
+  const [open, setOpen] = React.useState(false)
+  const [change, setChange] = React.useState(false)
 
-  console.log(arr);
+  console.log(arr)
   // React.useEffect(() => {
   //     // Update the document title using the browser API
   //     setArr(arr)
@@ -49,14 +46,24 @@ export default function Notes() {
   // const handleClick = () => {
   //   setOpen(true);
   // };
-
+  React.useEffect(() => {
+    axios
+      .post(
+        'http://localhost:8000/users/getmynotes',
+        {},
+        { headers: { auth: 'ahahah' } }
+      )
+      .then((res) => {
+        console.log(res.data)
+      })
+  }, [change])
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
-      return;
+      return
     }
 
-    setOpen(false);
-  };
+    setOpen(false)
+  }
   return (
     <div className={classes.root}>
       <h1>My Notes</h1>
@@ -73,29 +80,79 @@ export default function Notes() {
         {/* {arr.map((elem,index) => (
           <NoteCard arr={arr} setArr={setArr} elem={elem} index={index} />
         ))} */}
-          {arr.filter((e)=>e.isPinned).length===0?<div className={classes.noteSection}>{arr.sort((a, b) => b.curDate - a.curDate).map((elem,index) => (
-          <NoteCard arr={arr} setArr={setArr} elem={elem} index={index} setOpen={setOpen}/>
-        ))}</div>:
-        <>
-        <h2 className={classes.noteSection}>Pinned</h2>
-        <div className={classes.noteSection} >
-        {arr.sort((a, b) => b.curDate - a.curDate).map((elem,index) => (
-          elem.isPinned?<NoteCard arr={arr} setArr={setArr} elem={elem} index={index} setOpen={setOpen}/>:<></>
-        ))}
-        </div>
-        {arr.filter((e)=>!e.isPinned).length===0?<></>:<h2 className={classes.noteSection}>Others</h2>}
-        <div className={classes.noteSection}>
-        {arr.sort((a, b) => b.curDate - a.curDate).map((elem,index) => (
-          !elem.isPinned?<NoteCard arr={arr} setArr={setArr} elem={elem} index={index} setOpen={setOpen}/>:<></>
-        ))}
-        </div>
-        </>}
+        {arr.filter((e) => e.isPinned).length === 0 ? (
+          <div className={classes.noteSection}>
+            {arr
+              .sort((a, b) => b.curDate - a.curDate)
+              .map((elem, index) => (
+                <NoteCard
+                  arr={arr}
+                  setArr={setArr}
+                  elem={elem}
+                  index={index}
+                  setOpen={setOpen}
+                />
+              ))}
+          </div>
+        ) : (
+          <>
+            <h2 className={classes.noteSection}>Pinned</h2>
+            <div className={classes.noteSection}>
+              {arr
+                .sort((a, b) => b.curDate - a.curDate)
+                .map((elem, index) =>
+                  elem.isPinned ? (
+                    <NoteCard
+                      arr={arr}
+                      setArr={setArr}
+                      elem={elem}
+                      index={index}
+                      setOpen={setOpen}
+                    />
+                  ) : (
+                    <></>
+                  )
+                )}
+            </div>
+            {arr.filter((e) => !e.isPinned).length === 0 ? (
+              <></>
+            ) : (
+              <h2 className={classes.noteSection}>Others</h2>
+            )}
+            <div className={classes.noteSection}>
+              {arr
+                .sort((a, b) => b.curDate - a.curDate)
+                .map((elem, index) =>
+                  !elem.isPinned ? (
+                    <NoteCard
+                      arr={arr}
+                      setArr={setArr}
+                      elem={elem}
+                      index={index}
+                      setOpen={setOpen}
+                    />
+                  ) : (
+                    <></>
+                  )
+                )}
+            </div>
+          </>
+        )}
       </div>
       <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity={open===1||open===4?"success":"error"}>
-          {open===1?<>Note added successfully</>:open===2?<>Cannot add an empty note</>:open===3?
-          <>Cannot have an empty note</>:<>Note updated successfully
-          </>}
+        <Alert
+          onClose={handleClose}
+          severity={open === 1 || open === 4 ? 'success' : 'error'}
+        >
+          {open === 1 ? (
+            <>Note added successfully</>
+          ) : open === 2 ? (
+            <>Cannot add an empty note</>
+          ) : open === 3 ? (
+            <>Cannot have an empty note</>
+          ) : (
+            <>Note updated successfully</>
+          )}
         </Alert>
       </Snackbar>
       {/* <Snackbar open={open4} autoHideDuration={2000} onClose={handleClose}>
@@ -114,5 +171,5 @@ export default function Notes() {
         </Alert>
       </Snackbar> */}
     </div>
-  );
+  )
 }
